99cd6bab115631425ea2902e078e88b1cdbaaafd
LJHyeokk <clzlsqkq56@naver.com>
2022-03-25 23:47:32 +0900

    [add]: counseling

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -29,7 +29,7 @@ const TopNav = () => {
         </li>
         <li
           onClick={() => {
-            navigate(`/`);
+            navigate(`/counseling`);
           }}
         >
           상담문의
--- src/pages/Counseling/Counseling.js
+++ src/pages/Counseling/Counseling.js
@@ -1,7 +1,24 @@
+import { Top, Logo } from '../../components/MainTopBottomLogo';
+import TopNav from '../../components/TopNav';
+import { ImHome2 } from 'react-icons/im';
 import './Counseling.scss';
 
 const Counseling = () => {
-  return <div className="Counseling">상담문의 홈페이지</div>;
+  return (
+    <div className="Counseling">
+      <Top />
+      <Logo navi={'/'} />
+      <TopNav />
+      <div className="itemWrapper">
+        <div className="navi">
+          <ImHome2 />
+          <span>
+            {'>'} INTERIOR {'>'} Detail
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Counseling;
